d67a2eb21a0dee7511a861e65b3e2ddeea0f12e2
Elmax19 <maksim.kohnovich@mail.ru>
2022-04-07 12:28:08 +0300

    initial commit

---
+++ src/main/java/by/elmax19/volleyballtournaments/VolleyballTournamentsApplication.java
@@ -0,0 +1,13 @@
+package by.elmax19.volleyballtournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleyballTournamentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolleyballTournamentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/elmax19/volleyballtournaments/VolleyballTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.elmax19.volleyballtournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyballTournamentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
