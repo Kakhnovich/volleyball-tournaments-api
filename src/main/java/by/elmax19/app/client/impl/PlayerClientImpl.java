a8a845c85a81d59bf75c286bc35ccb8f1068d7b8
Elmax19 <maksim.kohnovich@mail.ru>
2022-04-13 16:15:51 +0300

    adding FeignClient

--- src/main/java/by/elmax19/app/client/impl/PlayerClientImpl.java
+++ src/main/java/by/elmax19/app/client/impl/PlayerClientImpl.java
@@ -2,10 +2,8 @@ package by.elmax19.app.client.impl;
 
 import by.elmax19.app.client.PlayerClient;
 import by.elmax19.app.model.dto.PlayerDto;
-import by.elmax19.app.model.dto.UriParameterDto;
-import by.elmax19.app.service.component.WebClientUtils;
+import by.elmax19.app.client.FeignClientUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,19 +11,9 @@ import java.util.List;
 @Component
 public class PlayerClientImpl implements PlayerClient {
     @Autowired
-    private WebClientUtils webClientUtils;
-    @Value("${volleyball-players.api.base-url}")
-    private String playersBaseUrl;
-    @Value("${volleyball-players.api.players.path}")
-    private String playersPath;
-    @Value("${volleyball-players.api.players.club-parameter-name}")
-    private String clubParameterName;
+    private FeignClientUtils feignClient;
 
     public List<PlayerDto> getPlayersByClub(String clubName) {
-        return webClientUtils.getListByParameters(
-                playersBaseUrl,
-                playersPath,
-                List.of(new UriParameterDto(clubParameterName, clubName)),
-                PlayerDto.class);
+        return feignClient.findPlayersByClub(clubName);
     }
 }
