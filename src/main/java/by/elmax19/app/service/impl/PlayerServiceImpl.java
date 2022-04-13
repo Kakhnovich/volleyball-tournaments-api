fc99ad5ab79d3ebdefafa4f67ecd007bb0b0f6f7
Elmax19 <maksim.kohnovich@mail.ru>
2022-04-13 14:29:03 +0300

    adding PlayerClient

--- src/main/java/by/elmax19/app/service/impl/PlayerServiceImpl.java
+++ src/main/java/by/elmax19/app/service/impl/PlayerServiceImpl.java
@@ -1,11 +1,9 @@
 package by.elmax19.app.service.impl;
 
+import by.elmax19.app.client.PlayerClient;
 import by.elmax19.app.model.dto.PlayerDto;
-import by.elmax19.app.model.dto.UriParameterDto;
 import by.elmax19.app.service.PlayerService;
-import by.elmax19.app.service.component.WebClientUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,20 +11,10 @@ import java.util.List;
 @Service
 public class PlayerServiceImpl implements PlayerService {
     @Autowired
-    private WebClientUtils webClientUtils;
-    @Value("${volleyball-players.api.base-url}")
-    private String playersBaseUrl;
-    @Value("${volleyball-players.api.players.path}")
-    private String playersPath;
-    @Value("${volleyball-players.api.players.club-parameter-name}")
-    private String clubParameterName;
+    private PlayerClient playerClient;
 
     @Override
     public List<PlayerDto> findPlayersByClub(String clubName) {
-        return webClientUtils.getListByParameters(
-                playersBaseUrl,
-                playersPath,
-                List.of(new UriParameterDto(clubParameterName, clubName)),
-                PlayerDto.class);
+        return playerClient.getPlayersByClub(clubName);
     }
 }
